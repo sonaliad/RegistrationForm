b993fe22c4fdcff823e741ceb70592ff7f9486ca
Sonali Purwar <>
2021-08-21 02:40:53 +0530

    Initial Push

---
+++ script.js
@@ -0,0 +1,113 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+let state = false;
+let isToggle = true;
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    var mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!validateEmail(emailValue)) {
+        setErrorFor(email, 'Enter an valid email address');
+        document.getElementById("email").placeholder = " ";
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Password cannot be empty');
+        document.getElementById("password").placeholder = " ";
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    //add error message inside small
+    small.innerText = message;
+
+    //add error class
+    formControl.className = 'form-control error';
+}
+
+$('#password').keyup(function() {
+    var password = $('#password').val();
+    if (checkStrength(password) == false) {
+        console.log("IN");
+    }
+});
+
+function toggle(){
+    if(state){
+        document.getElementById("password").setAttribute("type","password");
+        state = false;
+    }else{
+        document.getElementById("password").setAttribute("type","text")
+        state = true;
+    }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+function myFunction(show){
+    show.classList.toggle("fa-eye-slash");
+}
+
+function checkStrength(password) {
+    var strength = 0;
+    //If password contains both lower and uppercase characters.
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+        $('.low-upper-case').addClass('text-success');
+        $('.low-upper-case i').removeClass('fa-file-text').addClass('fa-check');
+        $('#popover-password-top').addClass('hide');
+    } else {
+        $('.low-upper-case').removeClass('text-success');
+        $('.low-upper-case i').addClass('fa-file-text').removeClass('fa-check');
+        $('#popover-password-top').removeClass('hide');
+    }
+
+    //If it has numbers and characters.
+    if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) {
+        $('.one-number').addClass('text-success');
+        $('.one-number i').removeClass('fa-file-text').addClass('fa-check');
+        $('#popover-password-top').addClass('hide');
+
+    } else {
+        $('.one-number').removeClass('text-success');
+        $('.one-number i').addClass('fa-file-text').removeClass('fa-check');
+        $('#popover-password-top').removeClass('hide');
+    }
+
+    //If it has one special character.
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+        $('.one-special-char').addClass('text-success');
+        $('.one-special-char i').removeClass('fa-file-text').addClass('fa-check');
+        $('#popover-password-top').addClass('hide');
+
+    } else {
+        $('.one-special-char').removeClass('text-success');
+        $('.one-special-char i').addClass('fa-file-text').removeClass('fa-check');
+        $('#popover-password-top').removeClass('hide');
+    }
+
+    if (password.length > 7) {
+        $('.eight-character').addClass('text-success');
+        $('.eight-character i').removeClass('fa-file-text').addClass('fa-check');
+        $('#popover-password-top').addClass('hide');
+
+    } else {
+        $('.eight-character').removeClass('text-success');
+        $('.eight-character i').addClass('fa-file-text').removeClass('fa-check');
+        $('#popover-password-top').removeClass('hide');
+    }
+}
